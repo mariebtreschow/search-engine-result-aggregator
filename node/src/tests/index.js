c143d41841f1102444301a1de8b337787e68dccc
Marie Treschow <marie.treschow@mobgen.com>
2019-03-21 21:55:34 +0100

    renaming folers

--- node/src/tests/index.js
+++ node/src/tests/index.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
 const assert = require('chai').assert;
-
 const aggregator = require('../aggregator.js');
 
 describe('Should be able to extract title and url from google and yahoo search enginge result', () => {
@@ -10,22 +9,18 @@ describe('Should be able to extract title and url from google and yahoo search e
         assert.isTrue(aggregator.validateSearchEngines(array));
         done();
     });
-
     it('should only be able to search for google and yahoo not bing', (done) => {
         let array = ['bing', 'yahoo'];
         assert.isFalse(aggregator.validateSearchEngines(array));
         done();
     });
-
     it('should only be able to search for google and yahoo not bing', (done) => {
         let string = 'horse';
         assert.isTrue(aggregator.validateKeyword(string));
         done();
     });
-
     it('should be able to get a response from google and yahoo only', (done) => {
         aggregator.search(['Google', 'Yahoo'], 'Horse').then((results) => {
-            // console.log(results)
             _.each(results, (result) => {
                 assert.property(result, 'title');
                 assert.property(result, 'source');
